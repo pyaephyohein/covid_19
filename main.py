7b79b3e72ae3de3de9cd166783f6365f5ef010fa
Pyae Phyo Hein <40478662@live.napier.ac.uk>
2020-04-10 23:35:34 +0630

    add table

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ countries_api = requests.get("https://api.covid19api.com/summary")
 # confirmed_api = requests.get(f"/dayone/country/{country}/status/{status}/live")
 country_list = json.loads(countries_api.text)
 app = Flask(__name__)
-@app.route('/')
+@app.route('/list')
 def home():
-    return render_template('index.html', country_name = [ cname for  cname in (country_list['Countries'])] )
+    return render_template('list.html', country_name = [ cname for  cname in (country_list['Countries'])] )
 if  __name__ == "__main__":
     app.run(debug=True)
