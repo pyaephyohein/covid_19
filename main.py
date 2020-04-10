9cb5e79c476519653465f55a1e1d70b20bf10b4d
Pyae Phyo Hein <40478662@live.napier.ac.uk>
2020-04-11 00:29:25 +0630

    add index

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ countries_api = requests.get("https://api.covid19api.com/summary")
 country_list = json.loads(countries_api.text)
 app = Flask(__name__)
 @app.route('/list')
-def home():
+def list():
     return render_template('list.html', country_name = [ cname for  cname in (country_list['Countries'])] )
+@app.route('/')
+def home():
+    return render_template('index.html', global_data = [ glol for glol in (country_list['Date'][0:10])])
 if  __name__ == "__main__":
     app.run(debug=True)
